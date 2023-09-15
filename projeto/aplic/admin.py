985ec20b29a71034037c911c0ac8392d4a8e3514
jackson51472 <53409735+jackson51472@users.noreply.github.com>
2023-09-15 17:08:06 -0300

    Atualizção no Cliente,Funcionario e foi adicionado a classe Cargo

--- projeto/aplic/admin.py
+++ projeto/aplic/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from aplic.models import Pessoa
-@admin.register(Pessoa)
-class PessoaAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cpf', 'telefone')
+from aplic.models import Cliente, Cargo, Funcionario
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['cartao','nome','cpf','login','senha','telefone']
+    
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('nome_cargo', 'salario', 'comissao')
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['cargo','nome','cpf','login','senha','telefone']
--- projeto/aplic/models.py
+++ projeto/aplic/models.py
@@ -2,11 +2,30 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class Pessoa(models.Model):
-    nome = models.CharField(_('Nome'), max_length=100)
-    cpf = models.CharField(_("cpf"), blank=False, max_length=11)
-    telefone = models.CharField(_('telefone'), blank=True, max_length=200)
+    
+    nome = models.CharField(_("Nome"), blank=False, max_length=50,)
+    cpf = models.CharField(_("cpf"), blank=False, max_length=11,)
+    telefone = models.IntegerField(_('Telefone'), blank=True,)
+    senha = models.CharField(_('Senha'), blank=False, max_length=100)
+    login = models.CharField(_('Login'), blank=False, max_length=100)
+    
+    def alterar_nome(self):
+        novo_nome = models.CharField(_("Nome"), blank=False, max_length=50,)
+        self.nome = novo_nome
+    
+    def alterar_login(self):
+        novo_login = models.CharField(_('Login'), blank=False, max_length=100)
+        self.login = novo_login
+        
+    def alter_senha(self):
+        nova_senha = models.CharField(_('Senha'), blank=False, max_length=100)
+        self.senha = nova_senha
+        
+    def adicionar_telefone(self):
+        novo_telefone = models.IntegerField(_('Telefone'), blank=True,)
 
-    class Meta:
+    class Meta:    
+        abstract = True
         verbose_name = _('Pessoa')
         verbose_name_plural = _('Pessoas')
         ordering = ['id']
@@ -14,16 +33,33 @@ class Pessoa(models.Model):
     def __str__(self):
         return self.nome
 
-class Curso(models.Model):
-    nome = models.CharField(_('Nome'), max_length=100)
-    descricao = models.TextField(_('Descrição'), max_length=500)
-    carga_horaria = models.IntegerField(_('Carga Horária'))
-    #imagem = StdImageField(_('Imagem'), null=True, blank=True, upload_to=get_file_path, variations={'thumb': {'width': 420, 'height': 260, 'crop': True}})
+class Cliente(Pessoa):
+    cartao = models.CharField(_('Cartão'), max_length=20)
+
+    class Meta:
+        verbose_name = _('Cliente')
+        verbose_name_plural = _('Clientes')
+
+
+class Cargo(models.Model):
+    nome_cargo = models.CharField(_('Nome Cargo'), max_length=100)
+    salario = models.DecimalField(_("Salario"), blank=False, max_digits=12, decimal_places=3)
+    comissao = models.DecimalField(_("Comissão"), blank=False,max_digits=4, decimal_places=3)
+
 
 
     class Meta:
-        verbose_name = _('Curso')
-        verbose_name_plural = _('Cursos')
+        verbose_name = _('Cargo')
+        verbose_name_plural = _('Cargos')
+    
+    def __str__(self):
+        return self.nome_cargo
+class Funcionario(Pessoa):
+    cargo = models.ForeignKey(Cargo, null=True, on_delete= models.SET_NULL)
+
+    class Meta:
+        verbose_name = _('Funcionario')
+        verbose_name_plural = _('Funcionarios')
 
     def __str__(self):
-        return self.nome
+        return f"{self.nome} / {self.cargo}"
