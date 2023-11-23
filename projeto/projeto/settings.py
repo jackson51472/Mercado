ecc21ba1cff002b53ddfc9e90e89caee649ae84d
jnmorais <jsmorais2002@gmail.com>
2023-11-23 00:11:28 -0300

    terminaando de vincular o css

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "projeto\static"),
+    os.path.join(BASE_DIR, "aplic\static"),
     # '/var/www/static/',
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
